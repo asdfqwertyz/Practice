f95339fe4a2b36a777a11d6fb5728529d5f3d82c
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-02 20:31:44 +0300

    Updated practice-01.js

--- practice-01.js
+++ practice-01.js
@@ -1,43 +1,7 @@
-function showInfo(num) {
-  let flag;
-  if (num > 0) {
-    flag = 0;
-  } else if (num < 0) {
-    flag = 1;
+function getPow(num, degree) {
+  if (degree === 1) {
+    return num;
   }
-  if (
-    Math.trunc(num) / Math.pow(10, 1) >= 0.1 &&
-    Math.trunc(num) / Math.pow(10, 1) < 1
-  ) {
-    if (flag === 0) {
-      console.log(`${num} - положительно однозначное`);
-    } else {
-      console.log(`${num} - отрицательно однозначное`);
-    }
-  }
-  if (
-    Math.trunc(num) / Math.pow(10, 2) >= 0.1 &&
-    Math.trunc(num) / Math.pow(10, 2) < 1
-  ) {
-    if (flag === 0) {
-      console.log(`${num} - положительно двузначное`);
-    } else {
-      console.log(`${num} - отрицательно двузначное`);
-    }
-  }
-  if (
-    Math.trunc(num) / Math.pow(10, 3) >= 0.1 &&
-    Math.trunc(num) / Math.pow(10, 3) < 1
-  ) {
-    if (flag === 0) {
-      console.log(`${num} - положительно трехзначное`);
-    } else {
-      console.log(`${num} - отрицательно трехзначное`);
-    }
-  }
-  if (num === 0) {
-    console.log(`${num} - нуль`);
-  }
+  return num * getPow(num, degree - 1);
 }
-
-module.exports = showInfo
+module.exports = getPow
